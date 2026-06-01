cd47ceffbf3c5d74c4d8c4da1210efbaeb26f8ab
Amy Wang <amysonothere@gmail.com>
2026-06-01 15:15:33 -0600

    fix component import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import { Navbar } from "@/components/navbar";
 import Footer from "@/components/footer";
 
 const geistSans = Geist({
@@ -25,12 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main
+    <html
+      lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Navbar />
-      {children}
-      <Footer />
-    </main>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/landing page/hero";
 import Mission from "@/components/landing page/mission";
-import Navbar from "@/components/navbar";
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-black">
-      <Navbar />
       <Hero />
       <Mission />
     </main>
